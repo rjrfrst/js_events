8977a42ea8f111ebc12f91a9a82658f6c15b0111
Rayster Ramos <rjrfrst@gmail.com>
2023-11-27 14:38:02 +0000

    tried section 2

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,5 +1,19 @@
 const enterButton = document.querySelector("#enter");
+const toDoList = document.querySelector("#list");
+const newToDo = document.querySelector("#new-todo");
+const textOutput = document.querySelector("#text-output");
+
 
 enterButton.addEventListener("click", () =>{
-    console.log("button clicked")
+    console.log("button clicked");
+});
+
+toDoList.addEventListener("input", (event) => {
+    textOutput.innerText = event.target.value;
+
+    const newItem = document.createElement('li'); //Empty 
+    newItem.innerText = event.target.value;
+
+    list.appendChild(newItem); //Add empty 
+
 });
